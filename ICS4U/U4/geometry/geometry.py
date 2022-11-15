050c97d175e6f4f618b7a0764970867f9629fd4a
Ryan A <alumkalryan@gmail.com>
2022-11-14 21:12:24 -0500

    changes

--- ICS4U/U4/geometry/geometry.py
+++ ICS4U/U4/geometry/geometry.py
@@ -1,44 +1,37 @@
-#import main
-import math
+#IMPORTS 
+import math 
 
-def area_rectangle(length, width):
-    rectangle_area = round(length * width,2)
+def area_rectangle(length, width): #calculates area of rectangle 
+    rectangle_area = length * width
     return rectangle_area
 
-def area_circle(radius):
-    circle_area = round(math.pi*radius**2,2)
+def area_circle(radius): #calculates area of circle
+    circle_area = math.pi*radius**2
     return circle_area 
 
-def circumference(radius):
-    circle_circumference = round(2 * math.pi * radius,2)
+def circumference(radius): #calculates circumference of circle 
+    circle_circumference = 2 * math.pi * radius
     return circle_circumference
 
-def volume_rectangular_prism(length,width,height):
-    rectangular_prism_volume = round(length * width * height,2)
+def volume_rectangular_prism(length,width,height): #calculates volume of rectangular prism
+    rectangular_prism_volume = length * width * height
     return rectangular_prism_volume
 
-def volume_cylinder(radius,height):
-    cylinder_volume = round(math.pi * radius**2 * height,2)
+def volume_cylinder(radius,height): #calculates volume of cylinder 
+    cylinder_volume = math.pi * radius**2 * height
     return cylinder_volume
 
-def surface_area_rectangular_prism(length, width, height):
-    rectangle_area = area_rectangle(length, width)
-    rectangular_prism_volume = volume_rectangular_prism(length, width, height)
-    print(f"The area of the rectangle is {rectangle_area}")
-    print(f"The volume of the rectangle is {rectangular_prism_volume}")
-    surface_area = round(2*(rectangle_area + (height * length) + (height * width)),2)
-    print(f"The surface area of the rectangle is {surface_area}")
-    #call area_rectangle
+def surface_area_rectangular_prism(length, width, height): #calculates surface area of rectangle 
+    rectangle_area1 = area_rectangle(length, width) #area of rectangle 
+    rectangle_area2 = area_rectangle(length,height) #area of rectangle 
+    rectangle_area3 = area_rectangle(width,height) #area of rectangle 
+    rectangular_prism_surface_area = (2*rectangle_area1 + 2*rectangle_area2 + 2*rectangle_area3) #calculates surface area from area(s) of rectangle
+    return  rectangular_prism_surface_area
 
-def surface_area_cylinder(height,radius):
-    circle_area = area_circle(radius)
-    circle_circumference = circumference(radius)
-    rectangle_area = area_rectangle(circle_circumference, height)
-    cylinder_volume = volume_cylinder(radius, height)
-    print(f"The area of the circle is {circle_area}")
-    print(f"The circumference of the circle is {circle_circumference}")
-    print(f"The volume of the cylinder is {cylinder_volume}")
-    surface_area = round((2*circle_area) + (rectangle_area),2)
-    print(f"The surface area of the cylinder is {surface_area}")
-   
-    #call area_circle, area_rectangle, and circumference
+def surface_area_cylinder(height,radius): #calculates surface area of cylinder 
+    circle_area = area_circle(radius) #area of circle 
+    circle_circumference = circumference(radius) #circumference of circle
+    rectangle_area = area_rectangle(circle_circumference, height) #area of the "rectangular circle" part of the cylinder
+    cylinder_surface_area = ((2*circle_area) + (rectangle_area)) #calculates surface area from previous measurements 
+    return cylinder_surface_area  
+  
--- ICS4U/U4/geometry/main.py
+++ ICS4U/U4/geometry/main.py
@@ -1,37 +1,95 @@
 # Name - Ryan Alumkal
 # Grade - 12
-# Description - 
-# Date -  October 31, 2022
+# Description - Calculates various values given whether the user wants to calculate values for a rectangular prism or cylinder
+# Date -  November 15, 2022
 
-import geometry
+import geometry #imports calculations from geometry.py
 
-def choice():
+def user_shape(): #user's desired shape
     while True:
         try:
-            choice = int(input("Do you want to calculate for a rectangular prism(1) or cylinder(2): "))
-            if choice == 1 or choice == 2:
+            choice = int(input("Do you want to calculate for a rectangular prism(1) or cylinder(2): ")) #user input
+            if choice == 1 or choice == 2: #if choice is valid
                 break
-        except:
+            else: #if user input is invalid 
+                print("Enter a proper value, [1] for rectangular prism or [2] for cylinder")
+        except: #if user input is invalid 
             print("Enter a proper value, [1] for rectangular prism or [2] for cylinder")
     return choice
 
-def user_input(choice):
-    while True:
-        try:
-            if choice == 1: #if rectangle 
-                width = int(input("Enter the width: "))
-                length = int(input("Enter the length: "))
-                height = int(input("Enter the height: ")) 
-                geometry.surface_area_rectangular_prism(length, width, height)      
-            if choice == 2: #if cylinder
-                radius = int(input("Enter the radius: "))
-                height = int(input("Enter the height: ")) 
-                geometry.surface_area_cylinder(height,radius)
-        except:
-            print("Print a correct integer value")
+def user_input(choice): #asks user for measurements 
+    if choice == 1: #if rectangle 
+        while True:
+            try:
+                width = float(input("Enter the width: "))
+                length = float(input("Enter the length: "))
+                height = float(input("Enter the height: ")) 
+                if width > 0 and length > 0 and height >0: #if valid
+                    break
+                else: #if invalid 
+                    print("Print a correct float value")  
+            except: #if invalid 
+                print("Print a correct float value")  
+        values = [width, length, height] #measurements for a rectangular prism
+    else: #if cylinder
+        while True:
+            try:
+                radius = float(input("Enter the radius: "))
+                height = float(input("Enter the height: ")) 
+                if radius > 0 and height >0: #if valid 
+                    break
+                else: #if invalid 
+                    print("Print a correct float value")
+            except: #if invalid 
+                print("Print a correct float value")
+        values = [radius, height] #measurements for a cylinder 
+    return values #returns measurements 
 
-def main():
-    user_choice = choice()
-    user_input(user_choice)
-if __name__ == "__main__":
+def results(choice, values): #prints results of the measurements for various values 
+    if choice == 1: #if rectangular prism
+        rectangle_area1 = round(geometry.area_rectangle(values[1], values[0]),2) #area of rectangle; values = length and width
+        rectangle_area2 = round(geometry.area_rectangle(values[1],values[2]),2)  #area of rectangle; values = length and height
+        rectangle_area3 = round(geometry.area_rectangle(values[0],values[2]),2) #area of rectangle; values = width and height
+        rectangular_prism_volume = round(geometry.volume_rectangular_prism(values[1], values[0], values[2]),2) #rectangular prism volume; values = length, width, height
+        rectangular_prism_surface_area = round(geometry.surface_area_rectangular_prism(values[1], values[0], values[2]),2) # rectangular prism surface area; values = length, width, and height
+        print(f"The area of the rectangle with measurements {values[1]} and {values[0]} is {rectangle_area1}") #prints area of rectangle with measurements of length and width
+        print(f"The area of the rectangle with measurements {values[1]} and {values[2]} is {rectangle_area2}") #prints area of rectangle with measurements of length and height
+        print(f"The area of the rectangle with measurements {values[0]} and {values[2]} is {rectangle_area3}") #prints area of rectangle with measurements of width and height
+        print(f"The volume of the rectangular prism is {rectangular_prism_volume}") #prints volume of rectangular prism
+        print(f"The surface area of the rectangle is {rectangular_prism_surface_area}") #prints surface area of rectangular prism
+    else: #if cylinder 
+        circle_area = round(geometry.area_circle(values[0]),2) #area of circle; value = radius
+        circle_circumference = round(geometry.circumference(values[0]),2) #circumference of circle; value = radius 
+        cylinder_volume = round(geometry.volume_cylinder(values[0], values[1]),2) #volume of cylinder; values = radius and height
+        cylinder_surface_area  = round(geometry.surface_area_cylinder(values[1], values[0]),2) #surface area of cylinder; values = height and radius
+        print(f"The area of the circle is {circle_area}") #prints area of circle 
+        print(f"The circumference of the circle is {circle_circumference}") #prints circumference of circle 
+        print(f"The volume of the cylinder is {cylinder_volume}") #prints volume of cylinder 
+        print(f"The surface area of the cylinder is {cylinder_surface_area }") #prints surface area of cylinder
+   
+def user_continue(): #if user wants to continue or not 
+    while True: #loop
+        try:
+            option = str(input("Do you want to continue [Y/N]: "))   #user choice 
+            if option == "Y" or option == "y" or option == "N" or option == "n": #if choice is valid 
+                break
+            else:
+                print("Enter a valid option [Y or N]") #if input is not valid 
+        except:
+            print("Enter a valid option [Y or N]") #if user input is not valid 
+    return option    
+
+def main(): #main function
+    while True: #while user wants to continue 
+        user_choice = user_shape() #user's desired shape
+        values = user_input(user_choice) #measurements 
+        results(user_choice, values) #values/results 
+        choice = user_continue() #if user wants to continue or not
+        if choice == "N" or choice == "n": #if user does not want to continue
+            print("Thank you for using the program") 
+            break #ends program
+
+#main program
+
+if __name__ == "__main__": 
     main()
