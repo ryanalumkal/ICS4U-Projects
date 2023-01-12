126fddd0eaeb0fe3149a9a7a25d54aacbd0c5b3f
Ryan A <82676662+ryanalumkal@users.noreply.github.com>
2023-01-12 11:05:05 -0500

    added comments and fixed u6 part 1

--- ICS4U/U6/Part 1/ICS3UfactorialSum.py
+++ ICS4U/U6/Part 1/ICS3UfactorialSum.py
@@ -1,29 +1,46 @@
 # Name - Ryan Alumkal
 # Grade - 12
-# Description - Calculates the sum of all factorials entered by a user
-# Date -  
+# Description - Calculates the sum of all factorials entered by the user
+# Date -  1/12/2023
 
-
-
-#Ask if user wants to continue 
 #modules 
-import mathematics
+import calculations
 
-def user_input():
-	nums = []
+def user_input(): #asks user for input
+	nums = [] #numbers in a list 
 	while True:
-		try:
-			x = int(input("Enter a number: "))
-			nums.append(x)
-			if x == 0:
+		try: #checks if user input is correct
+			x = int(input("Enter a number (enter 0 to stop): ")) #asks user input
+			if x >= 0: #if value is greater or equal to 0 (equal to 0 used when user only inputs 0 and no other value)
+				nums.append(x) #adds to the list "nums"
+			elif x < 0: #if value is 0 or less 
+				print("Enter a correct input")
+			if x == 0: #if value is 0
 				return nums
 				break
 		except:
 			print("Enter a proper value")
+			
+def user_continue(): #if user wants to continue or not 
+    while True: #loop
+        try:
+            option = input("Do you want to continue [Y/N]: ").lower()   #user choice 
+            if option == "y" or option == "yes" or option == "n" or option == "no": #if choice is valid 
+                break
+            else:
+                print("Enter a valid option [Y or N]") #if input is not valid 
+        except:
+            print("Enter a valid option [Y or N]") #if user input is not valid 
+    return option   	
 	
-def main():
-	nums = user_input()
-	print(f"The sum of the factorials is {mathematics.factorial(nums)}")
+def main(): #main function
+	while True:
+		nums = user_input() #gets the user input 
+		print(f"The sum of the factorials is {calculations.factorial(nums)}") #ouputs user input using a module from calculations.py (attached file)
+		choice = user_continue() #user choice (if they want to continue)
+		if choice == "n" or choice == "no": #if user does not want to continue
+			print("Thank you for using the program") 
+			break #ends program
 
 #main program
 if __name__ == "__main__": 
