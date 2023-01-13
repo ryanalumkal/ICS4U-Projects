f708b863b0156cbb5c717cdd0381d605bc3f9336
Ryan A <alumkalryan@gmail.com>
2023-01-12 21:55:54 -0500

    added U7 part 1 and studentdata.txt file

---
+++ U7/Part 1/main.py
@@ -0,0 +1,12 @@
+# Name - Ryan Alumkal
+# Grade - 12
+# Description - 
+# Date -  
+
+def main():
+    file = open(r'studentdata.txt','r')
+    print(file.read())
+
+if __name__ == "__main__":
+    main()
+
